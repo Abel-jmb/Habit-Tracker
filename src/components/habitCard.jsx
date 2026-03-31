bfb2ceee46f00e9d366f04aa84bca7eaf34d705c
Abel Montaño <abelmontap18@gmail.com>
2026-03-31 12:48:46 -0500

    feat: add checkbox and delete button for habits

--- src/components/habitCard.jsx
+++ src/components/habitCard.jsx
@@ -1,10 +1,16 @@
-const HabitCard = ({ habit, toggleHabit, deleteHabit  }) => {
-
+const HabitCard = ({ habit, toggleHabit, deleteHabit }) => {
   return (
-    <article >
-        <header>
-            <h3>{habit.name}</h3>
-        </header>
+    <article>
+      <header>
+        <h3>{habit.name}</h3>
+      </header>
+      <div>
+        <label htmlFor="completed">
+          <input type="checkbox" name="completed" checked={habit.completed} onChange={() => toggleHabit(habit.id)}/>
+        </label>
+
+      <button onClick={() => deleteHabit(habit.id)}>Eliminar habito</button>
+      </div>
     </article>
   );
 };
