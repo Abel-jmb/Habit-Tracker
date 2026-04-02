5dcfe5ddf65d9965327eeaa1663bfeb31ef680b6
Abel Montaño <abelmontap18@gmail.com>
2026-04-02 17:21:42 -0500

    style: add styles to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
     return (
-        <header>
-            <h1>Habit Tracker</h1>
-            <button>Tema</button>
+        <header className="flex justify-between items-center bg-purple-900 text-white p-4">
+            <h1 className="text-2xl font-bold">Habit Tracker</h1>
+            <button>🌙</button>
         </header>
     );
 }
